098792e554dcbfafd46169d52cb9f2719c47da51
Miguel Arreaza <mg.arreaza.13@gmail.com>
2021-04-22 17:04:01 -0400

    feat: update home with new components

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ImgSlider from "../imgSlider";
+import NewDisney from "../newDisney";
+import Originals from "../originals";
+import Recommends from "../recommends";
+import Trending from "../trending";
 import Viewers from "../viewers";
 import { Container } from "./styles";
 
@@ -8,6 +12,10 @@ function Home() {
     <Container>
       <ImgSlider />
       <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
   );
 }

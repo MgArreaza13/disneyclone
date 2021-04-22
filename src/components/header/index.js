ac84934e412687286bcbcc5a98eb2f05ce45ec8e
Miguel Arreaza <mg.arreaza.13@gmail.com>
2021-04-22 11:39:38 -0400

    feat: add redux to project

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,43 @@
 import React from "react";
-import { Nav, Logo, NavMenu, Login } from "./styles";
+import { Nav, Logo, NavMenu, Login, UserImg } from "./styles";
 import items from "./items";
-import {auth, provider} from '../../firebase'
-
+import { auth, provider } from "../../firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import {
+  selectUserName,
+  selectUserEmail,
+  selectUserPhoto,
+  setUserLoginDetails,
+} from "../../features/user/userSlice";
 
 function Header() {
-
-
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const userName = useSelector(selectUserName);
+  const userEmail = useSelector(selectUserEmail);
+  const userPhoto = useSelector(selectUserPhoto);
 
   const handleAuth = () => {
-    auth.signInWithPopup(provider).then((result => {
-      console.log(result);
-    })).catch((error) => {
-      console.log(error)
-    })
-  }
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-
+  const setUser = (user) => {
+    dispatch(
+      setUserLoginDetails({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      })
+    );
+  };
 
   const NavItem = ({ item, key }) => {
     return (
@@ -32,12 +53,19 @@ function Header() {
       <Logo>
         <img src="images/logo.svg" alt="logo" />
       </Logo>
-      <NavMenu>
-        {items.map((item, index) => {
-          return <NavItem item={item} key={index} />;
-        })}
-      </NavMenu>
-      <Login onClick={handleAuth}>Login</Login>
+
+      {!userName ? (
+        <Login onClick={handleAuth}>Login</Login>
+      ) : (
+        <>
+          <NavMenu>
+            {items.map((item, index) => {
+              return <NavItem item={item} key={index} />;
+            })}
+          </NavMenu>
+          <UserImg src={userPhoto} alt={userName} />
+        </>
+      )}
     </Nav>
   );
 }
